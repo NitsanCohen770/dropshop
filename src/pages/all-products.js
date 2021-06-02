d4235fc524bbbc0943272ece989dd204026df2ef
nitsancohen770 <nitsancohen770@gmail.com>
2021-06-02 13:09:35 +0300

    product description added

---
+++ src/pages/all-products.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../components/';
+
+const AllProducts = () => {
+  return (
+    <Layout>
+      <h1>All Products</h1>
+    </Layout>
+  );
+};
+
+AllProducts.propTypes = {};
+
+export default AllProducts;
